71ea2f1462155e14b003c7582054df308ece141c
nhatnq2310 <nhatn@DESKTOP-O3ACKEA>
2025-10-20 00:17:11 +0700

    finish IOrderService logic

--- EVMDealerSystem.DataAccess/Repository/Interfaces/IOrderService.cs
+++ EVMDealerSystem.DataAccess/Repository/Interfaces/IOrderService.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EVMDealerSystem.BusinessLogic.Commons;
+using EVMDealerSystem.BusinessLogic.Models.Request.Order;
+using EVMDealerSystem.BusinessLogic.Models.Responses;
 
 namespace EVMDealerSystem.DataAccess.Repository.Interfaces
 {
-    internal interface IOrderService
+    public interface IOrderService
     {
+        Task<Result<OrderResponse>> CreateOrderAsync(OrderCreateRequest request, Guid dealerStaffId);
+        Task<Result<OrderResponse>> GetOrderByIdAsync(Guid id);
+        Task<Result<IEnumerable<OrderResponse>>> GetOrdersByStaffAsync(Guid staffId);
+        Task<Result<IEnumerable<AvailableVehicleForOrder>>> GetAvailableVehiclesAsync(Guid dealerId);
+        Task<Result<decimal>> CalculateOrderPriceAsync(Guid vehicleId, Guid? promotionId);
     }
 }
