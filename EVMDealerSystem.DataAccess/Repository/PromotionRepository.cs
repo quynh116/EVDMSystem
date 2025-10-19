d68625926e12ef5e8f73e1020ce8beb31b9c1655
nhatnq2310 <nhatn@DESKTOP-O3ACKEA>
2025-10-19 23:46:15 +0700

    finish PromotionRepository logic

--- EVMDealerSystem.DataAccess/Repository/PromotionRepository.cs
+++ EVMDealerSystem.DataAccess/Repository/PromotionRepository.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EVMDealerSystem.DataAccess.Models;
+using EVMDealerSystem.DataAccess.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace EVMDealerSystem.DataAccess.Repository
 {
-    internal class PromotionRepository
+    public class PromotionRepository : IPromotionRepository
     {
+        private readonly EVMDealerSystemContext _context;
+
+        public PromotionRepository(EVMDealerSystemContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Promotion?> GetByIdAsync(Guid id)
+        {
+            return await _context.Promotions
+                .Include(p => p.Vehicle)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<Promotion?> GetActiveByVehicleIdAsync(Guid vehicleId)
+        {
+            var now = DateTime.Now;
+            return await _context.Promotions
+                .Where(p => p.VehicleId == vehicleId &&
+                           p.IsActive &&
+                           p.StartDate <= now &&
+                           p.EndDate >= now)
+                .FirstOrDefaultAsync();
+        }
     }
 }
