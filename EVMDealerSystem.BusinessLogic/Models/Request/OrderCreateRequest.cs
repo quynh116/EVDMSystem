acfa09dacb22401669987890ec1ace05a824b8e0
nhatnq2310 <nhatn@DESKTOP-O3ACKEA>
2025-10-19 23:28:18 +0700

    finish OrderCreateRequest logic

--- EVMDealerSystem.BusinessLogic/Models/Request/OrderCreateRequest.cs
+++ EVMDealerSystem.BusinessLogic/Models/Request/OrderCreateRequest.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace EVMDealerSystem.BusinessLogic.Models.Request
 {
-    internal class OrderCreateRequest
+    public class OrderCreateRequest
     {
+        [Required]
+        public Guid CustomerId { get; set; }
+
+        [Required]
+        public Guid VehicleId { get; set; }
+
+        [Required]
+        public Guid InventoryId { get; set; }
+
+        public string? PaymentType { get; set; }
+
+        public Guid? PromotionId { get; set; }
+
+        public string? Note { get; set; }
     }
 }
